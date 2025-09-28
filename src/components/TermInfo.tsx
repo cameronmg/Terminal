9d2c6fc3ec600d3106443be4c96097acc03c360b
cameronmg <cam@DESKTOP-76E1NEV.localdomain>
2025-09-28 18:55:10 -0500

    "Fixing live page"

--- src/components/TermInfo.tsx
+++ src/components/TermInfo.tsx
@@ -3,7 +3,7 @@ import { User, WebsiteName, Wrapper } from "./styles/TerminalInfo.styled";
 const TermInfo = () => {
   return (
     <Wrapper>
-      <User>friend</User>@<WebsiteName>github.com/cameronmg</WebsiteName>:~$
+      <User>friend</User>@<WebsiteName>cameronmg.github.io/aboutme/</WebsiteName>:~$
     </Wrapper>
   );
 };
